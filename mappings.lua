861e363dc647677a38e35ce1c06d9e766651a7fb
kevinsmith115 <kevindsmith115@gmail.com>
2023-04-12 20:45:37 -0500

    added lua install and todo plugin

--- mappings.lua
+++ mappings.lua
@@ -20,6 +20,8 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+  
+    ["<leader>a"] = { "<cmd>echo 'Hello friend.'<cr>", desc = "Say Hello friend " },
   },
   t = {
     -- setting a mapping to false will disable it
